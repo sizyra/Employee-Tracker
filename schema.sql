7619e93b6c11cf8e4eb1e1778e3801a2a6eb6b30
Sam Zyra <sisaaczyra@gmail.com>
2020-09-29 12:05:58 -0700

    Created MySQL schema

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
